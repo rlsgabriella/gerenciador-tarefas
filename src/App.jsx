bf8faa338b0aa9b468855ee12ce25eee7aad21c9
Gabriella Rodrigues <rlsgabriella@gmail.com>
2025-01-07 18:23:37 -0300

    iniciando estudos react e criando gerenciador de tarefas

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+
+import { useState } from "react";
+import AddTask from "./Components/AddTask";
+import Tasks from "./Components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Revisar lógica de programação",
+      description: "Revisar lógica para desenvolver mais habilidades",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AddTask.jsx
@@ -0,0 +1,7 @@
+function AddTask() {
+
+<h1>AddTask</h1>
+
+}
+
+export default AddTask
